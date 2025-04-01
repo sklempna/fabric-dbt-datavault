3c65df2fe45f8aff1edcb696fdefa729603e4a90
sklempna <60576241+sklempna@users.noreply.github.com>
2025-04-01 16:04:31 +0200

    rm pits

--- demoproject/models/business_vault/control_snap_v0.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='incremental') }}
-
-{%- set yaml_metadata -%}
-start_date: '2025-03-30'
-daily_snapshot_time: '07:30:00'
-{%- endset -%}    
-
-{{ datavault4dbt.control_snap_v0(yaml_metadata=yaml_metadata) }} 
